f3b4cd1491f14538dfb918b96ea27f8375807047
Blacksider1 <adamtsurov123@gmail.com>
2026-01-07 20:11:03 +0300

    add filtreing by categories

---
+++ src/constants/categories.js
@@ -0,0 +1,9 @@
+export const NEWS_CATEGORIES = [
+  { id: 'business', label: 'Business' },
+  { id: 'entertainment', label: 'Entertainment' },
+  { id: 'general', label: 'General' },
+  { id: 'health', label: 'Health' },
+  { id: 'science', label: 'Science' },
+  { id: 'sports', label: 'Sports' },
+  { id: 'technology', label: 'Technology' },
+];
