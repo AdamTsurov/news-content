f4538371ffb366ca69f18bb231774503a477c4b2
Blacksider1 <adamtsurov123@gmail.com>
2026-01-12 12:04:01 +0300

    add design for web

---
+++ src/componets/BannersList/BannersList.jsx
@@ -0,0 +1,23 @@
+import withSkeleton from '../../helpers/hocs/withSkeleton';
+import NewsBanner from '../NewsBanner/NewsBanner';
+import styles from './styles.module.css';
+
+const BannersList = ({ banners }) => {
+  if (!banners) {
+    return <div>Нет новостей</div>;
+  }
+
+  return (
+    <ul className={styles.banners}>
+      {banners?.map(banner => {
+        return (
+          <NewsBanner key={banner.id} item={banner}/>
+        )
+      })}
+    </ul>
+  );
+};
+
+const BannersListWithSkeleton = withSkeleton(BannersList, 'banner', 10, 'row');
+
+export default BannersListWithSkeleton;
