4d7362e300b0d90a846663fcc0a5d92f2a4d4d0e
Blacksider1 <adamtsurov123@gmail.com>
2026-01-19 16:50:36 +0300

    migrate from js to ts

--- src/componets/Categories/Categories.tsx
+++ src/componets/Categories/Categories.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { type ForwardedRef } from 'react';
 import styles from './styles.module.css';
+import type { CategoriesType } from '../../interfaces';
+
+interface Props {
+  categories: CategoriesType[];
+  setSelectedCategory: (category: CategoriesType | null) => void;
+  selectedCategory: CategoriesType | null;
+}
 
 const Categories = React.forwardRef(
-  ({ categories, setSelectedCategory, selectedCategory }, ref) => {
+  ({ categories, setSelectedCategory, selectedCategory }: Props, ref: ForwardedRef<HTMLDivElement>) => {
     return (
       <div ref={ref} className={styles.categories}>
         <button
